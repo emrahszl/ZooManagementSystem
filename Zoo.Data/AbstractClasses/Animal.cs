53cf2804834abb951efb6a11a74d1ac9c9395930
Emrah Sözlü <emrahsozlu@hotmail.com>
2023-05-27 00:08:41 +0300

    Added initial class, interface, and enum definitions

---
+++ Zoo.Data/AbstractClasses/Animal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zoo.Data.Enums;
+
+namespace Zoo.Data.AbstractClasses
+{
+    public abstract class Animal
+    {
+        public Gender Gender { get; set; }
+        public int Age { get; set; }
+        public abstract void Feeding();
+        public abstract void MakeSound();
+    }
+}
